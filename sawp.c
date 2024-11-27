cf6aa21c48ce224c9c814fe65282b964292cd40a
nihadkhan-1815 <nihadkhan1815@gmail.com>
2024-11-26 23:08:46 -0800

    Create sawp.c

---
+++ sawp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void swap(intx,inty)
+{
+    int temp=x;
+    x=y;
+    y=temp;
+}
+int main()
+{
+    int a=5,b=10;
+    swap(a,b0;
+    printf("a=%d,b=%\n",a,b);
+return 0;
+}
