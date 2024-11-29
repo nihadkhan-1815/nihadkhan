74051385a94198f0db81650c3f58b38be69c0b70
nihadkhan-1815 <nihadkhan1815@gmail.com>
2024-11-29 11:02:08 +0530

    Update

---
+++ structure 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ struct student {
+     char name;
+    int age;
+    float department name;
+    float college name;
+ };
+int main() {
+    struct student s;
+    printf("enter name");
+    scanf("%s,name");
+    printf("enter age");
+    scand("%d",&s age);
+    printf("enter department name");
+    scanf("%f",&s department name);
+    printf("enter College name");
+    scanf("%a",&s college name);
+    printf("name,%s\nage:%d department name:%f, college name:%a,")
+    
+    return 0;
+}
